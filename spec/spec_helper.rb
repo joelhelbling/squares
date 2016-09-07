8f075ccfc6a8da8c1cd8664df4078d7b2164c93f
Joel Helbling <joel@joelhelbling.com>
2016-09-07 10:11:21 -0400

    adding code climate token to travis.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+require 'squares'
 require 'pry'
 require 'rspec-given'
-require 'squares'
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
 
 # load fixtures
 Dir['spec/fixtures/**/*.rb'].each do |fixture|
