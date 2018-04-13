99d0b17064c2659ea77aca4da9b59144f409720c
Joel Helbling <joel@joelhelbling.com>
2018-04-13 10:04:42 -0400

    a bit of hook refactoring

--- lib/squares/hooks.rb
+++ lib/squares/hooks.rb
@@ -8,15 +8,33 @@ module Squares
 
     module InstanceMethods
       def trigger hook_name
-        return if @hook_callback_in_progress
+        return if hook_callback_in_progress?
         hooks = self.class.hooks
         if hooks && hooks[hook_name]
           hooks[hook_name].each do |hook|
-            @hook_callback_in_progress = true
-            self.instance_eval(&hook)
-            @hook_callback_in_progress = false
+            hook_callback { self.instance_eval(&hook) }
           end
         end
+      end
+
+      private
+
+      def hook_callback_in_progress?
+        @hook_callback_in_progress
+      end
+
+      def hook_callback(&block)
+        begin_hook_callback!
+        block.call
+        end_hook_callback!
+      end
+
+      def begin_hook_callback!
+        @hook_callback_in_progress = true
+      end
+
+      def end_hook_callback!
+        @hook_callback_in_progress = false
       end
     end
 
